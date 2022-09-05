30f6f38a87edd450808ad6555af64f12eef4cdf2
hyemi <jessie8021@gmail.com>
2022-09-05 15:40:25 +0900

    포스트에 태그 기능 추가

--- about_hyemi/blog/models.py
+++ about_hyemi/blog/models.py
@@ -13,6 +13,16 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
         db_table = 'categories'
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'Tags'
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -23,11 +33,11 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     author = models.ForeignKey(CustomUser, null=True, on_delete=models.SET_NULL)
-
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     class Meta:
-        db_table = 'posts'
+        db_table = 'Posts'
 
 
     def __str__(self):
