1ff206a3f15c57e349b8ec88156c128facd5f4cb
hyemi <jessie8021@gmail.com>
2022-09-05 11:40:38 +0900

    포스트 이미지 파일 업로드

--- about_hyemi/blog/urls.py
+++ about_hyemi/blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', views.get_PostList),
     path('<int:pk>/', views.get_Post),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
